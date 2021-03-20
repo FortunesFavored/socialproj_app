6e75eab3a3a2598ab7cfe702b97156481998f3ba
katerijy <katerijy@yahoo.com>
2021-03-19 21:59:53 -0700

    fri night

---
+++ src/views/createChapter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class createChapter extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/views/story2.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class story2 extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
